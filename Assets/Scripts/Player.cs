968cf196da7c1d79c70563f7a9ef659c9a702d3e
Leonardo Gomide <leocgomide@gmail.com>
2022-05-04 20:14:00 -0300

    GardeniaButton

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,21 +5,43 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Entity entity;
+    private GameObject gardeniaButton;
 
     [Header("Player Regeneration")]
     public bool regenerateHp = true;
+
     public int regenerateHPValue = 5;
+
     public int regenerateHPTime = 2;
 
     [Header("Game Manager")]
-
     public GameManager manager;
 
+    bool CheckCloseToTag(string tag, float minimumDistance)
+    {
+        GameObject[] goWithTag = GameObject.FindGameObjectsWithTag(tag);
+
+        for (int i = 0; i < goWithTag.Length; ++i)
+        {
+            if (
+                Vector3
+                    .Distance(transform.position,
+                    goWithTag[i].transform.position) <=
+                minimumDistance
+            ) return true;
+        }
+
+        return false;
+    }
+    
     // [Header("Player UI")]
     // Start is called before the first frame update
     void Start()
     {
-        if(manager == null){
+        gardeniaButton = GameObject.Find("GardeniaButton");
+        
+        if (manager == null)
+        {
             Debug.LogError("instancie o gameManager para essa entidade");
             return;
         }
@@ -28,31 +50,44 @@ public class Player : MonoBehaviour
         entity.maxMana = manager.CalculateMana(this);
         entity.maxStamina = manager.CalculateStamina(this);
 
-        int dmg = (int)manager.CalculateDamage(this, 7);
-        int def = (int)manager.CalculateDefence(this, 4);
+        int dmg = (int) manager.CalculateDamage(this, 7);
+        int def = (int) manager.CalculateDefence(this, 4);
 
         entity.currentMana = 1;
         entity.currentHealth = entity.maxHealth;
         entity.currentStamina = entity.maxStamina;
 
         StartCoroutine(RegenerateHealth());
-        
     }
 
-    IEnumerator RegenerateHealth(){
-        while(true){
-            if(regenerateHp){
-                if(entity.currentHealth < entity.maxHealth){
-                    
+    void Update()
+    {
+        gardeniaButton.SetActive(CheckCloseToTag("gardenia", 30) && !GameObject.Find("PauseMenu"));
+        
+        // if (CheckCloseToTag("gardenia", 30)) {
+        //     Debug.Log(gardeniaButton);
+        // }
+    }
+
+    IEnumerator RegenerateHealth()
+    {
+        while (true)
+        {
+            if (regenerateHp)
+            {
+                if (entity.currentHealth < entity.maxHealth)
+                {
                     Debug.LogFormat("Recuperando hp");
                     entity.currentHealth += regenerateHPValue;
                     yield return new WaitForSeconds(regenerateHPTime);
-                    
-                }else{
+                }
+                else
+                {
                     yield return null;
                 }
             }
-            else{
+            else
+            {
                 yield return null;
             }
         }
