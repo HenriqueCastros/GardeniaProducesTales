eb83fda00f1cda7e2d315fcc3b1495eb31d92559
Leonardo Gomide <leocgomide@gmail.com>
2022-05-31 20:50:32 -0300

    FIXED MAIN MENU😘

--- maingame/Assets/Scripts/PauseMenu.cs
+++ maingame/Assets/Scripts/PauseMenu.cs
@@ -6,20 +6,28 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     static GameObject youDied;
+
     static GameObject youWon;
-    
-    void Start() {
-        youDied = youDied != null ? youDied : GameObject.Find("You Died");
-        if (youDied != null) youDied.SetActive(false);
-        
-        youWon = youWon != null ? youWon : GameObject.Find("You Won");
-        if (youWon != null) youWon.SetActive(false);
+
+    void Start()
+    {
+        if (youDied == null)
+        {
+            youDied = youDied != null ? youDied : GameObject.Find("You Died");
+            if (youDied != null) youDied.SetActive(false);
+        }
+
+        if (youWon == null)
+        {
+            youWon = youWon != null ? youWon : GameObject.Find("You Won");
+            if (youWon != null) youWon.SetActive(false);
+        }
     }
-    
+
     static void SetPlayerMoviment(bool active)
     {
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        
+
         for (int i = 0; i < players.Length; ++i)
         {
             players[i].GetComponent<PlayerControler>().allowMoviment = active;
@@ -42,21 +50,23 @@ public class PauseMenu : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
-    
+
     public void GoToCredits()
     {
         SceneManager.LoadScene("Credits");
     }
 
-    public static void ActivateYouDied() {
+    public static void ActivateYouDied()
+    {
         SetPlayerMoviment(false);
         youDied.SetActive(true);
-    } 
+    }
 
-    public static void ActivateYouWon() {
+    public static void ActivateYouWon()
+    {
         SetPlayerMoviment(false);
         youWon.SetActive(true);
-    } 
+    }
 
     public void ReloadLevel()
     {
